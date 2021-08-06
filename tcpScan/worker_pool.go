220a1279e1dad9412588bf10403d1a6c76690598
Farmer.Chillax <farmer-chong@qq.com>
2021-08-06 22:43:56 +0800

    chore: add a go scanner

--- tcpScan/worker_pool.go
+++ tcpScan/worker_pool.go
@@ -10,7 +10,7 @@ import (
 func worker(ports chan int, results chan int) {
 	for p := range ports {
 		// fmt.Println(p)
-		address := fmt.Sprintf("xiaotao2333.top:%d", p)
+		address := fmt.Sprintf("farmer233.top:%d", p)
 		conn, err := net.Dial("tcp", address)
 		if err != nil {
 			results <- 0
@@ -23,8 +23,9 @@ func worker(ports chan int, results chan int) {
 
 func main() {
 	start := time.Now()
-	ports := make(chan int, 100)
-	results := make(chan int, 100)
+	ports := make(chan int, 30000)
+	results := make(chan int, 2000)
+	scanEndPort := 65535
 	var openPorts []int
 	var closePorts []int
 
@@ -32,30 +33,38 @@ func main() {
 	for i := 0; i < cap(ports); i++ {
 		go worker(ports, results)
 	}
-
 	// 启动worker
 	go func() {
-		for i := 1; i < 65535; i++ {
+		for i := 1; i < scanEndPort; i++ {
 			ports <- i
 		}
 	}()
 
 	// 处理worker
-	for i := 1; i < 65535; i++ {
-		port := <-results
-		if port != 0 {
-			openPorts = append(openPorts, port)
+	// for i := 1; i < 65535; i++ {
+	// 	port := <-results
+	// 	if port != 0 {
+	// 		openPorts = append(openPorts, port)
+	// 	} else {
+	// 		closePorts = append(closePorts, port)
+	// 	}
+	// }
+
+	for i := 1; i < scanEndPort; i++ {
+		res := <-results
+		if res != 0 {
+			openPorts = append(openPorts, res)
 		} else {
-			closePorts = append(closePorts, port)
+			closePorts = append(closePorts, res)
 		}
 	}
-	// wg.Wait()
+
 	close(ports)
 	close(results)
 	sort.Ints(openPorts)
-	for _, port := range closePorts {
-		fmt.Printf("%d closed\n", port)
-	}
+	// for _, port := range closePorts {
+	// 	fmt.Printf("%d closed\n", port)
+	// }
 	for _, port := range openPorts {
 		fmt.Printf("%d opend\n", port)
 	}
