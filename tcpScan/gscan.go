96f5f76f5ea16aaef7ecec5c00e972401e576dde
Farmer.Chillax <farmer-chong@qq.com>
2021-08-08 13:50:18 +0800

    Chore: 添加go scanner

--- tcpScan/gscan.go
+++ tcpScan/gscan.go
@@ -1,34 +1,26 @@
 package main
 
-import (
-	"fmt"
-	"log"
-	"net"
-	"sync"
-	"time"
-)
-
-func main() {
-	start := time.Now()
-	var wg sync.WaitGroup
-	for i := 21; i < 1024; i++ {
-		wg.Add(1)
-		go func(port int) {
-			defer wg.Done()
-			address := fmt.Sprintf("192.168.2.123:%d", port)
-			conn, err := net.Dial("tcp", address)
-			if err != nil {
-				log.Printf("%s 关闭了\n", address)
-				return
-			}
-			conn.Close()
-			log.Printf("%s 打开了！!!!!!!!\n", address)
-		}(i)
-	}
-	wg.Wait()
-	elapsed := time.Since(start) / 1e9
-	log.Printf("\n\n%d seconds", elapsed)
-}
+// func main() {
+// 	start := time.Now()
+// 	var wg sync.WaitGroup
+// 	for i := 21; i < 1024; i++ {
+// 		wg.Add(1)
+// 		go func(port int) {
+// 			defer wg.Done()
+// 			address := fmt.Sprintf("192.168.2.123:%d", port)
+// 			conn, err := net.Dial("tcp", address)
+// 			if err != nil {
+// 				log.Printf("%s 关闭了\n", address)
+// 				return
+// 			}
+// 			conn.Close()
+// 			log.Printf("%s 打开了！!!!!!!!\n", address)
+// 		}(i)
+// 	}
+// 	wg.Wait()
+// 	elapsed := time.Since(start) / 1e9
+// 	log.Printf("\n\n%d seconds", elapsed)
+// }
 
 // func main() {
 // 	for i := 20; i < 1024; i++ {
