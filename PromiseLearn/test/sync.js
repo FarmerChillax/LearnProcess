c796ba624498e4ef2660ef451c47f4f70dcb594b
Farmer.Chillax <farmer-chong@qq.com>
2022-03-06 22:42:41 +0800

    chore: 测试sync

---
+++ PromiseLearn/test/sync.js
@@ -0,0 +1,11 @@
+for (var i = 0; i < 5; i++) {
+    setTimeout(() => {
+        console.log(`i = ${i}`)
+    }, 0);
+}
+
+for (let j = 0; j < 5; j++) {
+    setTimeout(() => {
+        console.log(`j = ${j}`)
+    }, 0);
+}
