f24fa40510206eebcdabcde616e2a9b68cd88b15
Farmer.Chillax <farmer-chong@qq.com>
2022-03-16 17:16:58 +0800

    chore: 更新DS结构, 添加python

---
+++ DataStruct/python/list/list_node.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+'''
+    :file: list_node.py
+    :author: -Farmer
+    :url: https://blog.farmer233.top
+    :date: 2022/03/16 11:41:24
+'''
+
+class Node():
+    def __init__(self, value, next=None) -> None:
+        self.value = value
+        self.next = next
+
+
