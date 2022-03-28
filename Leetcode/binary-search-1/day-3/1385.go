a8bd89ebe9a709223a468ba48332ba4a0ecb28ec
Farmer.Chillax <farmer-chong@qq.com>
2022-03-28 23:04:21 +0800

    chore: 添加二分day-3

---
+++ Leetcode/binary-search-1/day-3/1385.go
@@ -0,0 +1,24 @@
+package day3
+
+func findTheDistanceValue(arr1 []int, arr2 []int, d int) int {
+	ans := 0
+	for i := 0; i < len(arr1); i++ {
+		flag := true
+		for j := 0; j < len(arr2); j++ {
+			if abs(arr1[i]-arr2[j]) <= d {
+				flag = false
+			}
+		}
+		if flag {
+			ans++
+		}
+	}
+	return ans
+}
+
+func abs(x int) int {
+	if x < 0 {
+		return -x
+	}
+	return x
+}
--- Leetcode/binary-search-1/day-3/uni_test.go
+++ Leetcode/binary-search-1/day-3/uni_test.go
@@ -17,3 +17,26 @@ func Test367(t *testing.T) {
 	utils.HandleResult(res, except)
 
 }
+
+func Test1385(t *testing.T) {
+	input1 := []int{4, 5, 8}
+	input2 := []int{10, 9, 1, 8}
+	d := 2
+	except := 2
+	res := findTheDistanceValue(input1, input2, d)
+	utils.HandleResult(res, except)
+
+	input1 = []int{1, 4, 2, 3}
+	input2 = []int{-4, -3, 6, 10, 20, 30}
+	d = 3
+	except = 2
+	res = findTheDistanceValue(input1, input2, d)
+	utils.HandleResult(res, except)
+
+	input1 = []int{2, 1, 100, 3}
+	input2 = []int{-5, -2, 10, -3, 7}
+	d = 6
+	except = 1
+	res = findTheDistanceValue(input1, input2, d)
+	utils.HandleResult(res, except)
+}
