2697335ff2e596d5c52d53e93af5a2b427ce27ee
Farmer.Chillax <farmer-chong@qq.com>
2022-05-22 18:06:11 +0800

    chore: 添加context组织结构

--- go/go-senior-coding/rpc/server/main.go
+++ go/go-senior-coding/rpc/server/main.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+	"context"
+	"fmt"
 	"log"
 	"net"
+	"net/http"
+	_ "net/http/pprof"
 	"net/rpc"
 )
 
@@ -14,6 +18,10 @@ func (hs *HelloService) Hello(request string, reply *string) error {
 }
 
 func main() {
+	go func() {
+		log.Println("pprof star in :5122")
+		http.ListenAndServe(":5122", nil)
+	}()
 	rpc.RegisterName("HelloService", new(HelloService))
 
 	listener, err := net.Listen("tcp", ":9980")
@@ -21,13 +29,34 @@ func main() {
 		log.Fatal("ListenTCP error:", err)
 	}
 
+	ctx, cancel := context.WithCancel(context.Background())
+
+	go func() {
+		var input string
+		log.Println("RPC Service running in :9980 ...")
+		log.Println("Enter anything to cancel.")
+		fmt.Scanln(&input)
+		cancel()
+	}()
+
+	go StartService(ctx, listener)
+
+	<-ctx.Done()
+
+}
+
+func StartService(ctx context.Context, listener net.Listener) {
 	for {
-		conn, err := listener.Accept()
-		if err != nil {
-			log.Fatal("Accept error:", err)
+		select {
+		case <-ctx.Done():
+			return
+		default:
+			conn, err := listener.Accept()
+			if err != nil {
+				log.Fatal("Accept error:", err)
+			}
+
+			go rpc.ServeConn(conn)
 		}
-
-		go rpc.ServeConn(conn)
 	}
-
 }
