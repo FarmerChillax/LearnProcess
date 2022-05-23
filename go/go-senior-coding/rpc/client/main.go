727242117ee44b9a6415c11ee26d4a7fce68dd0a
Farmer.Chillax <farmer-chong@qq.com>
2022-05-23 19:03:15 +0800

    chore: add loop request

--- go/go-senior-coding/rpc/client/main.go
+++ go/go-senior-coding/rpc/client/main.go
@@ -4,21 +4,36 @@ import (
 	"fmt"
 	"log"
 	"net/rpc"
+	"sync"
 )
 
+var wg sync.WaitGroup
+
 func main() {
+
+	for i := 0; i < 100; i++ {
+		wg.Add(1)
+		go Client(i)
+	}
+	wg.Wait()
+}
+
+func Client(id int) {
+
 	client, err := rpc.Dial("tcp", "localhost:9980")
 	if err != nil {
 		log.Fatal("rpcDial err:", err)
 	}
 
-	var reply string
-	err = client.Call("HelloService.Hello", "farmer", &reply)
+	var userName, reply string
+	userName = fmt.Sprintf("caller %d", id)
+	err = client.Call("HelloService.Hello", userName, &reply)
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
 	fmt.Println(reply)
+	wg.Done()
 
 }
