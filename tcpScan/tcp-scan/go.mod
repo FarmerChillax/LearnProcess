926a7690bb9ee4778e97a40193ed8edd4667aae5
FarmerChillax <farmer-chong@qq.com>
2022-05-29 01:40:21 +0800

    feat: finish tcp port scan

--- tcpScan/tcp-scan/go.mod
+++ tcpScan/tcp-scan/go.mod
@@ -3,6 +3,7 @@ module tcp-scan
 go 1.15
 
 require (
+	github.com/fatih/color v1.13.0 // indirect
 	github.com/pkg/profile v1.6.0
-	github.com/urfave/cli/v2 v2.7.1 // indirect
+	github.com/urfave/cli/v2 v2.8.1
 )
--- tcpScan/tcp-scan/main.go
+++ tcpScan/tcp-scan/main.go
@@ -15,31 +15,23 @@ func main() {
 	defer profile.Start(profile.MemProfile, profile.MemProfileRate(1)).Stop()
 	// 加载环境变量
 	// ...
-	segmentA := flag.String("a", "192", "网段A")
-	segmentB := flag.String("b", "%d", "网段B")
-	segmentC := flag.String("c", "%d", "网段C")
-	segmentD := flag.String("d", "%d", "网段D")
+	host := flag.String("host", "127.0.0.1", "目标主机")
 	flag.Parse()
-	args := flag.Args()
-	fmt.Println(args)
-	fmt.Println(*segmentA, *segmentB, *segmentC, *segmentD)
+	// args := flag.Args()
+	// fmt.Println(args)
+	// fmt.Println(*host)
+
 	// 得到A-D网段
 	// segmentList := []string{"10", "9", "10"}
 	// 开始创建扫描任务
 	// ...
-
+	address := fmt.Sprintf("%s:%%d", *host)
+	pkg.StartPort(address)
 }
 
-func InitTask(endpoint string, taskChan chan<- pkg.Task, timeout ...time.Duration) {
-	task := pkg.NewTask(endpoint, timeout...)
-	taskChan <- *task
-}
+// func create(segments []string) {
+// 	switch len(segments) {
+// 	case 3:
 
-func worker(tasks, result chan pkg.Task) {
-	for task := range tasks {
-		if task.Error = task.Do(); task.Error == nil {
-			task.Status = true
-		}
-		result <- task
-	}
-}
+// 	}
+// }
